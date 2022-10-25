057b52ed3c715db188e2a6e2d921b82432dac7f9
Andreas Martin <andreas.martin@andreasmartin.ch>
2022-10-25 20:08:33 +0200

    Update

--- init.sh
+++ init.sh
@@ -29,5 +29,5 @@ git remote add -f origin https://github.com/AI4BP/ainotes.git
 echo "$opt/" >> .git/info/sparse-checkout
 echo ".gitignore" >> .git/info/sparse-checkout
 git pull origin main
-cp ./$opt/init/Dockerfile ./
-cp ./$opt/init/requirements.txt ./
+cp ./$opt/init/requirements.txt ./ > /dev/null || :
+cp ./$opt/init/Dockerfile ./ > /dev/null || :
